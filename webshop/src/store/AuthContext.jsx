21b732a17a0ddb11ee57166dfc5897f2b13bff13
Mihkel Vaher <mihkel.vaher@industry62.com>
2023-12-28 17:01:59 +0200

    auth firebases

--- webshop/src/store/AuthContext.jsx
+++ webshop/src/store/AuthContext.jsx
@@ -10,18 +10,28 @@ export function AuthContextProvider({ children }) {
 
   const url = "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=" + process.env.REACT_APP_FIREBASE_WEB_API_KEY; // URL täita
 
+  // timeout?  ringi käies logiks ka välja
+  // peita URL-d
+
+  // kontrollid / validations - kui tuleb tühjus refresh tokeniga
+
   useEffect(() => {
     if (sessionStorage.getItem("expiresIn") === null ||
-    sessionStorage.getItem("token") === null ||
     sessionStorage.getItem("refreshToken") === null) {
+      sessionStorage.clear();
       return;
     }
-    if (Date.now() > Number(sessionStorage.getItem("expiresIn"))) {
+    if (true || Date.now() > Number(sessionStorage.getItem("expiresIn"))) {
       updateIdToken();
+    }
+    if (sessionStorage.getItem("token") === null) {
+      sessionStorage.clear();
+      return;
     }
     const payload = {
       "idToken": sessionStorage.getItem("token")
     }
+    // siia fetchi ilma ülemise await-ta läheks korraga
     fetch(url, {"method": "POST", "body": JSON.stringify(payload)})
       .then(res => res.json())
       .then(json => {
@@ -38,20 +48,33 @@ export function AuthContextProvider({ children }) {
 
   const refreshTokenUrl = "https://securetoken.googleapis.com/v1/token?key=" + process.env.REACT_APP_FIREBASE_WEB_API_KEY;
 
-  const updateIdToken = () => {
+  const updateIdToken = async () => {
+    console.log("UPDATE-n Tokenit")
     const payload = {
       "refresh_token": sessionStorage.getItem("refreshToken"),
       "grant_type": "refresh_token"
     }
-    fetch(refreshTokenUrl, {"method": "POST", "body": JSON.stringify(payload)})
-      .then()
-      .then()
+    // fetch on alati async ehk asünkroonne, lubab koodil edasi minna
+    await fetch(refreshTokenUrl, {"method": "POST", "body": JSON.stringify(payload)})
+      .then(res => res.json())
+      .then(json => {
+        if (json.error) {
+          console.log("CLEARING kui refreshtokenit saates error");
+          sessionStorage.clear();
+          return;
+        }
+        login(json.id_token, json.refresh_token, json.expires_in);
+      })
   }
 
   const login = (idToken, refreshToken, expiresIn) => {
+    console.log(idToken);
+    console.log(refreshToken);
+    console.log(expiresIn);
     setIsLoggedIn(true);
     let milliseconds = Date.now();
-    milliseconds += Number(expiresIn);
+    milliseconds += Number(expiresIn) * 1000;
+    console.log(milliseconds);
     sessionStorage.setItem("expiresIn", milliseconds);
     sessionStorage.setItem("token", idToken);
     sessionStorage.setItem("refreshToken", refreshToken);
